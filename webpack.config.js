c0110301ea8e8aea64637980364d91ae6820a8bb
Dayvid Lima <dwlima@gmail.com>
2017-11-14 16:12:31 -0300

    hot replacement

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,27 @@
 const path = require('path');
+const webpack = require('webpack');
 
 const config = {
-  entry: './app/index.js',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/',
-  },
+    entry: './app/index.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/',
+    },
 };
 
 if (process.env.NODE_ENV === 'development') {
-  config.watch = true;
-  config.devtool = 'source-map';
+    config.watch = true;
+    config.devtool = 'source-map';
+} else if (process.env.NODE_ENV === 'hot') {
+    config.devtool = 'source-map';
+    config.devServer = {
+        hot: true,
+    };
+    // plugin para o webpack
+    config.plugins = [
+        new webpack.HotModuleReplacementPlugin(),
+    ]
 }
 
 module.exports = config;
